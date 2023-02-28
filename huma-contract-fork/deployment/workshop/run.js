818adfd9ef53155962826dff92bd1609c501a06a
Tao Wang <taowang@Taos-MacBook-Pro.local>
2023-02-28 00:13:51 -0700

    Add the huma-contract-fork part

---
+++ huma-contract-fork/deployment/workshop/run.js
@@ -0,0 +1,51 @@
+const {getDeployedContracts} = require("../utils.js");
+
+const ZERO_BYTES32 = "0x0000000000000000000000000000000000000000000000000000000000000000";
+const salt = ethers.utils.formatBytes32String("salt");
+
+let deployer, deployedContracts;
+
+async function adjustFees() {
+    if (!deployedContracts["BaseCreditPoolFeeManager"]) {
+        throw new Error("BaseCreditPoolFeeManager not deployed yet!");
+    }
+
+    const FeeManager = await hre.ethers.getContractFactory("BaseFeeManager");
+    const feeManager = FeeManager.attach(deployedContracts["BaseCreditPoolFeeManager"]);
+
+    await feeManager.setFees(
+        20_000_000, // flat originatetion fee to be 10 usdc
+        5000, // proportional originatetion fee to be 50%
+        10_000_000, // flat late fee to be 10 usdc
+        5000, // proportional late fee to be 50%
+        0 // membership fee to be 0 usdc/month
+    );
+
+    if (!deployedContracts["BaseCreditPoolConfig"]) {
+        throw new Error("BaseCreditPoolConfig not deployed yet!");
+    }
+
+    const PoolConfig = await hre.ethers.getContractFactory("BasePoolConfig");
+    const poolConfig = PoolConfig.attach(deployedContracts["BaseCreditPoolConfig"]);
+
+    await poolConfig.setAPR(1000); // setting apr to be 10%
+}
+
+async function main() {
+    const network = (await hre.ethers.provider.getNetwork()).name;
+    console.log("network : ", network);
+    const accounts = await hre.ethers.getSigners();
+    deployer = await accounts[0];
+    console.log("deployer address: " + deployer.address);
+
+    deployedContracts = await getDeployedContracts();
+
+    await adjustFees();
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
