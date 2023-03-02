0cb9d59c7cdda52d9ddbc7285fb4947711711501
justin <justinzhou.web3@gmail.com>
2023-03-02 08:27:08 -0700

    update huma contract

--- huma-contract-fork/contracts/openzeppelin/TimelockController.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v4.7.0) (governance/TimelockController.sol)
-
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/governance/TimelockController.sol";
--- huma-contract-fork/deployment/mainnet/verify_args/ReceivableFactoringPool.js
+++
@@ -1,3 +0,0 @@
-module.exports = [
-        '0xBB1d6Ca4ffa64b3b3FD4Cb8C0476737cE1026AE7','0x3b1b39C039cB25701fc708cC8984DC49b0dEc55b',[],
-        ];
